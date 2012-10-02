7309ef9fb4f82bbba909541c9179d44861d340d9
AndreyUtka <AndreyUtka@gmail.com>
2012-10-02 15:47:53 +0300

    update project

--- amichaluik/Algorithms/InsertionSort/Program.cs
+++ amichaluik/Algorithms/InsertionSort/Program.cs
@@ -1,23 +1,56 @@
-﻿namespace InsertionSort
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsertionSort
 {
     class Program
     {
-        static void Main()
-        {   
+        static void Main(string[] args)
+        {
             var massive = new[] { 5, 2, 4, 6, 1, 3 };
 
-            var result = GetInsertionSort(massive);
+            ViewValue(GetInsertionSortByOrderOfGrowth(massive));
+
+            ViewValue(GetInsertionSortByOrderOforderDecrease(massive));
+
+            Console.ReadLine();
         }
 
-        private static int[] GetInsertionSort(int[] massive)
+        private static void ViewValue(IEnumerable<int> result)
         {
-            for (var j = 1; j < massive.Length; j++)
+            Console.WriteLine();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static IEnumerable<int> GetInsertionSortByOrderOfGrowth(IList<int> massive)
+        {
+            for (var j = 1; j < massive.Count; j++)
             {
                 var key = massive[j];
 
                 var i = j - 1;
 
                 while (i > -1 && massive[i] > key)
+                {
+                    massive[i + 1] = massive[i];
+                    i = i - 1;
+                    massive[i + 1] = key;
+                }
+            }
+            return massive;
+        }
+        private static IEnumerable<int> GetInsertionSortByOrderOforderDecrease(IList<int> massive)
+        {
+            for (var j = 1; j < massive.Count; j++)
+            {
+                var key = massive[j];
+
+                var i = j - 1;
+
+                while (i > -1 && massive[i] < key)
                 {
                     massive[i + 1] = massive[i];
                     i = i - 1;
