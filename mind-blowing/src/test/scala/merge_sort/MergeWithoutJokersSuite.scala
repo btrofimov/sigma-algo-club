a5557320857b89ffc91fc9f70ad436b7d955bdd7
Ivan Masich <w3cvalid@gmail.com>
2012-10-23 22:01:33 +0300

    Added more tests for negative numbers.

--- mind-blowing/src/test/scala/merge_sort/MergeWithoutJokersSuite.scala
+++ mind-blowing/src/test/scala/merge_sort/MergeWithoutJokersSuite.scala
@@ -20,4 +20,17 @@ class MergeWithoutJokersSuite extends FunSuite {
   test("List(5,10,11,1,2)") {
     assert(merge(List(5,10,11,1,2), 0, 2, 4) === List(1,2,5,10,11))
   }
+  
+  test("List(5,10,10,1,2,3)") {
+    assert(merge(List(5,10,10,1,2,3), 0, 2, 5) === List(1,2,3,5,10,10))
+  }
+  
+  
+  test("List(-11,-10,-5,-3,-2,-1)") {
+    assert(merge(List(-11,-10,-5,-3,-2,-1), 0, 2, 5) === List(-11,-10,-5,-3,-2,-1))
+  }
+  
+  test("List(5,10,11,-3,-2,-1)") {
+    assert(merge(List(5,10,11,-3,-2,-1), 0, 2, 5) === List(-3,-2,-1,5,10,11))
+  }
 }
