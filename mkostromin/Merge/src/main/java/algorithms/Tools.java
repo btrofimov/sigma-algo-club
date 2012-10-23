811da974ea4858523e020b9f7c37319674a71611
Maksim Kostromin <daggerok@gmail.com>
2012-10-23 13:30:52 +0300

    Merge project (maven/eclipse) implementation and unit tests.

---
+++ mkostromin/Merge/src/main/java/algorithms/Tools.java
@@ -0,0 +1,28 @@
+package algorithms;
+
+public class Tools {
+    public static void merge(int[] numbers) {
+        int lLen = numbers.length / 2;
+        int rLen = numbers.length - lLen;
+        int[] left = new int[lLen];
+        int[] right = new int[rLen];
+        int i = 0;
+
+        while (i < lLen) {
+            left[i] = numbers[i++];
+        }
+        for (int j = 0; j < rLen; j++) {
+            right[j] = numbers[i++];
+        }
+
+        int lId, rId;
+        for (lId = rId = 0, i = 0; i < numbers.length; i++) {
+            if (lId < lLen && rId < rLen) {
+                numbers[i] = left[lId] < right[rId] ? left[lId++] : right[rId++];
+            } else {
+                numbers[i] = lId == lLen ? right[rId++] : left[lId++];
+            }
+
+        }
+    }
+}
---
+++ mkostromin/Merge/src/test/java/algorithms/ToolsTest.java
@@ -0,0 +1,67 @@
+package algorithms;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.junit.Assert;
+
+public class ToolsTest extends TestCase {
+    public ToolsTest(String testName) {
+        super(testName);
+    }
+
+    public static Test suite() {
+        return new TestSuite(ToolsTest.class);
+    }
+
+    public void testMerge() {
+        int[] actuals = new int[] { 1, 2, 3, 4, 5, 6 };
+        int[] expecteds = new int[] { 1, 2, 3, 4, 5, 6 };
+
+        Tools.merge(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+
+        actuals = new int[] { 1, 2, 3, 4, 5 };
+        expecteds = new int[] { 1, 2, 3, 4, 5 };
+
+        Tools.merge(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+
+        actuals = new int[] { -1, -2, -3, -4, -5 };
+        expecteds = new int[] { -3, -4, -5, -1, -2 };
+
+        Tools.merge(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+
+        actuals = new int[] { 6, 5, 4, 3, 2, 1 };
+        expecteds = new int[] { 3, 2, 1, 6, 5, 4 };
+
+        Tools.merge(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+
+        actuals = new int[] { 5, 4, 3, 2, 1 };
+        expecteds = new int[] { 3, 2, 1, 5, 4 };
+
+        Tools.merge(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+
+        actuals = new int[] { -5, -4, -3, -2, -1 };
+        expecteds = new int[] { -5, -4, -3, -2, -1 };
+
+        Tools.merge(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+
+        actuals = new int[] { 1, 8, 3, 6, 5 };
+        expecteds = new int[] { 1, 3, 6, 5, 8 };
+
+        Tools.merge(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+
+        actuals = new int[] { 1, -8, 3, -6, 5 };
+        expecteds = new int[] { 1, -8, 3, -6, 5 };
+
+        Tools.merge(actuals);
+        Assert.assertArrayEquals(expecteds, actuals);
+    }
+}
